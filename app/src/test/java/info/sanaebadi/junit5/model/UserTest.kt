be3a029c4e8c06266b3f36314874855996d9bd4d
Sana Ebadi <sanaebadi97@gmail.com>
2020-04-16 13:44:25 -0400

    test user class complete

---
+++ app/src/test/java/info/sanaebadi/junit5/model/UserTest.kt
@@ -0,0 +1,40 @@
+package info.sanaebadi.junit5.model
+
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertAll
+
+class UserTest {
+    @Test
+    fun testMultiplePropertiesOfUser() {
+        val user = User(
+            firstName = "Hoge",
+            lastName = "Foobar",
+            age = 21,
+            postalAddresses = listOf(
+                "361 yellow  road", "randoville", "tehran"
+            )
+
+        )
+
+        assertAll("Test a user",
+            { assertNotNull(user.firstName) },
+            { assertNotNull(user.lastName) },
+            {
+                assertAll("Age-related tests",
+                    { assertEquals(21, user.age) },
+                    { assertTrue(user.isAdult()) }
+
+
+                )
+
+            },
+            {
+                assertAll("Address tests",
+                   // { assertEquals(1, user.postalAddresses) },
+                    { assertEquals(1, user.postalAddresses) },
+                    { assertTrue(user.canReceivedMail()) })
+            })
+    }
+}
