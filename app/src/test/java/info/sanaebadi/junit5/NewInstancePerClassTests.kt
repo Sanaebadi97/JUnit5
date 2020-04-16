d6ea5d02c76c2abc3ea45e468bf55f469e9b8ec2
Sana Ebadi <sanaebadi97@gmail.com>
2020-04-16 09:42:55 -0400

    added PER_CLASS class test

---
+++ app/src/test/java/info/sanaebadi/junit5/NewInstancePerClassTests.kt
@@ -0,0 +1,28 @@
+package info.sanaebadi.junit5
+
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.TestInstance
+
+
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+class NewInstancePerClassTests {
+
+    @Test
+    fun test1() {
+        println("Running Test 1 , this $this")
+    }
+
+
+    @Test
+    fun test2() {
+        println("Running Test 2 , this $this")
+    }
+
+
+    @Test
+    fun test3() {
+        println("Running Test 3 , this $this")
+    }
+
+
+}
