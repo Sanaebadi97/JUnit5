6a27bb17011e5e37fc5637d93c466d84bc806724
Sana Ebadi <sanaebadi97@gmail.com>
2020-04-16 12:00:46 -0400

    define assertEqual and assert same

--- app/src/test/java/info/sanaebadi/junit5/AssertionExample.kt
+++ app/src/test/java/info/sanaebadi/junit5/AssertionExample.kt
@@ -1,10 +1,17 @@
 package info.sanaebadi.junit5
 
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Assertions.assertNotEquals
+import org.junit.jupiter.api.Assertions.assertSame
 import org.junit.jupiter.api.Test
 
 class AssertionExample {
     @Test
     fun exampleAssertion() {
+        val string1 = "Hello World12"
+        val string2 = "Hello World"
 
+        assertNotEquals(string1, string2)
+        println("is not same")
     }
 }
