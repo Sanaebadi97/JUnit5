1313187b5bd58edfb40d896fb45bf1ee604891f1
Sana Ebadi <sanaebadi97@gmail.com>
2020-04-20 15:20:46 -0400

    added subtract method

--- app/src/main/java/info/sanaebadi/junit5/javaBrains/MathUtils.kt
+++ app/src/main/java/info/sanaebadi/junit5/javaBrains/MathUtils.kt
@@ -8,10 +8,13 @@ class MathUtils {
             return a + b
         }
 
+
+        fun subtract(a: Int, b: Int): Int {
+            return a - b
+        }
+
         fun computeCircleArea(radius: Double): Double {
-
             var area = Math.PI * radius * radius
-
             return area
         }
     }
